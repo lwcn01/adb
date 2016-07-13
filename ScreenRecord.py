f45f9189a6d661207b014a07f3b7b24c8fbb581f
new <lwcn01@163.com>
2016-07-13 14:01:08 +0800

    Update ScreenRecord.py

--- ScreenRecord.py
+++ ScreenRecord.py
@@ -6,14 +6,13 @@ from Utils.util import *
 Android 4.4+以上系统支持屏幕录制功能
 """
 adb = Adb()
-def screen_record():
+def screen_record(times):
+    global screen_dir
     timestamp = time.strftime('%Y-%m-%d_%H.%M.%S',time.localtime(time.time()))
     video_path = "/data/local/tmp/" + timestamp + ".mp4"
-    screen_dir = CreateDir('path2')
-    ScreenRecord(video_path).wait
-    num = input("回车键停止录制")
-    if num =='':
-        adb.adb('kill-server')
+    path = "--time-limit %s " %times + video_path
+    screen_dir = CreateDir('path3')
+    ScreenRecord(path).wait()
     Pull(video_path,screen_dir).wait()
     adb.shell("rm -r " + video_path)
 
@@ -22,8 +21,19 @@ if __name__ == "__main__":
     status = adb.DeviceStatus()
     if 'device' == status:
         if int(sdk_version) > 19:
-            screen_record()
-            print('录制成功 ---> ' + CreateDir('path2'))
+            times = input('屏幕录制时长：')
+            print('开始屏幕录制')
+            if times == '':
+                screen_record(180)
+                print('录制成功 ---> ' + screen_dir)
+            else:
+                try:
+                    times = int(times)
+                except Exception as e:
+                    print(e)
+                else:
+                    screen_record(int(times))
+                    print('录制成功 ---> ' + screen_dir)
         else:
             print('Android版本低于4.4，无法录制')
     else:
